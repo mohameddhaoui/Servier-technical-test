439c394fbd24ab7e58fbecb7e5e75fb29f59cd15
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 11:21:45 +0200

    (clean) update parameters name

--- lib/modules/data_prep/data_preprocessing.py
+++ lib/modules/data_prep/data_preprocessing.py
@@ -5,10 +5,26 @@ from lib.utils.tasks import generate_task_result, is_success
 from config import SUCCESS_STATUS
 
 
-def run_data_preprocessing_pipeline(df_filepath, datasrc_name, data_transformation_config, output_directory, dependency=False):
+def run_data_preprocessing_pipeline(df_filepath:str, datasrc_name:str, data_preprocessing_config:dict, output_directory:str, dependency:dict)-> dict:
+
+    """
+    Run Data preprocessing Pipeline : clean and structure columns
+
+        Params:
+        --------
+             df_file_path (str) : filepath to check
+             datasrc_name (str) : name of datasource
+             data_preprocessing_config (dict) : preprocessing dictionary
+             dependency (dict) : dependant task result
+
+        Returns:
+        ---------
+             result_quality_checks (dict) : A dictionary containing the quality check operations result
+
+    """
     if is_success(dependency) :
         try:
-            df_data_transformation= transform_data(df_filepath, datasrc_name, data_transformation_config)
+            df_data_transformation= transform_data(df_filepath, datasrc_name, data_preprocessing_config)
             output_preprocessed_filepath = f"{output_directory}{os.path.basename(df_filepath)}"
             save_df(df_data_transformation, output_preprocessed_filepath)
             task_result = output_preprocessed_filepath
@@ -19,24 +35,24 @@ def run_data_preprocessing_pipeline(df_filepath, datasrc_name, data_transformati
 
 
 
-def transform_data(df_filepath, datasrc_name, data_transformation_config):
+def transform_data(df_filepath, datasrc_name, data_preprocessing_config):
     df_data_transformation= load_data(df_filepath)
-    if "column_transformation" in data_transformation_config.keys():
+    if "column_transformation" in data_preprocessing_config.keys():
         df_data_transformation=transform_columns(df_data_transformation,
-                                                data_transformation_config=data_transformation_config["column_transformation"])
+                                                data_preprocessing_config=data_preprocessing_config["column_transformation"])
     df_data_transformation["type"] = datasrc_name
     return df_data_transformation
 
 
-def transform_columns(df, data_transformation_config):
-    if "remove_nans" in data_transformation_config.keys():
-        df = remove_nans(df, data_transformation_config["remove_nans"]["columns"])
-    if "remove_special_char" in data_transformation_config.keys():
-        df = remove_special_char(df, data_transformation_config["remove_special_char"]["columns"])
-    if "column_types" in data_transformation_config.keys():
-        for column_name, column_type in data_transformation_config["column_types"].items():
+def transform_columns(df, data_preprocessing_config):
+    if "remove_nans" in data_preprocessing_config.keys():
+        df = remove_nans(df, data_preprocessing_config["remove_nans"]["columns"])
+    if "remove_special_char" in data_preprocessing_config.keys():
+        df = remove_special_char(df, data_preprocessing_config["remove_special_char"]["columns"])
+    if "column_types" in data_preprocessing_config.keys():
+        for column_name, column_type in data_preprocessing_config["column_types"].items():
             df = cast_one_column(df, column_name,column_type)
-    if "column_rename" in data_transformation_config.keys():
-        for column_init_name, column_new_name in data_transformation_config["column_rename"].items():
+    if "column_rename" in data_preprocessing_config.keys():
+        for column_init_name, column_new_name in data_preprocessing_config["column_rename"].items():
             df = rename_one_column(df, column_init_name,column_new_name)
     return df
