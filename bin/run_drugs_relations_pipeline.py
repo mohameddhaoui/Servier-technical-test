bda839f1d0699f2f436e040154cd2c9f8e856b39
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 17:14:56 +0200

    (clean) add logging formatting

--- bin/run_drugs_relations_pipeline.py
+++ bin/run_drugs_relations_pipeline.py
@@ -9,9 +9,11 @@ if __name__ == "__main__":
 
     logger.info("-:-  STEP 1 - Run Data preparation pipeline ---------------------:-")
     data_preparation_output = run_data_prep(LIST_DATASRC)
+    logger.info(data_preparation_output)
 
     logger.info("-:-  STEP 2 - Run Data transformation pipeline -------------------:-")
     data_transformation_output = run_data_transformation(data_preparation_output)
+    logger.info(data_transformation_output)
 
     logger.info("-:-  STEP 3 - Run Data exposition pipeline ------------------------:-")
     data_exposition_output = run_data_exposition(data_transformation_output)
