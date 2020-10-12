dacfa8ef130d0cb083f7751a228db76fa3bde52b
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 14:49:30 +0200

    (update) add custom logger

--- lib/modules/data_transformation/data_transformation_pipeline.py
+++ lib/modules/data_transformation/data_transformation_pipeline.py
@@ -1,6 +1,7 @@
 from lib.modules.data_transformation.data_processing import (
     run_mentions_collection_pipeline,
 )
+from lib.helpers.logger import logger
 
 
 def run_data_transformation(data_prep_output: dict) -> dict:
@@ -15,6 +16,8 @@ def run_data_transformation(data_prep_output: dict) -> dict:
              data_processing_output (dict) : A dictionary containing the quality check operations result
 
     """
+    logger.info(f"-:----------- Run mention collection and processing  -:-")
     data_transformation_output = run_mentions_collection_pipeline(data_prep_output)
+    logger.info(data_transformation_output)
 
     return data_transformation_output
