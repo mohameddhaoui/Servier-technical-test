bff787977685f61879a3a9c2cfb4d042483aa352
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 19:20:57 +0200

    (doc) add docstring and black formating

--- lib/utils/tasks.py
+++ lib/utils/tasks.py
@@ -1,8 +1,15 @@
 from config import SUCCESS_STATUS
 
-def is_success(task_result):
-    return task_result["task_status"]== SUCCESS_STATUS
+
+def is_success(task_result: dict) -> bool:
+    """
+    Transform task result to bool
+    """
+    return task_result["task_status"] == SUCCESS_STATUS
 
 
-def generate_task_result( task_id="", task_status="", task_result=""):
-    return {"task_id":task_id, "task_status":task_status,"task_result":task_result}
+def generate_task_result(task_id="", task_status="", task_result="") -> dict:
+    """
+    Generate task result as dict
+    """
+    return {"task_id": task_id, "task_status": task_status, "task_result": task_result}
