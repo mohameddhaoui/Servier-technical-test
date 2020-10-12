cac23586e323be101994031b1331c6d430e93f56
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 13:18:46 +0200

    (feat) add data transformation pipeline

---
+++ lib/modules/data_transformation/data_transformation_pipeline.py
@@ -0,0 +1,16 @@
+from lib.modules.data_transformation.data_processing import run_mentions_collection_pipeline
+def run_data_transformation(data_prep_output:dict) -> dict:
+    """
+    Run data transformation pipeline
+        Params:
+        --------
+             data_prep_output (dict) : preprocessed files paths
+
+        Returns:
+        ---------
+             data_processing_output (dict) : A dictionary containing the quality check operations result
+
+    """
+    data_transformation_output = run_mentions_collection_pipeline(data_prep_output)
+
+    return data_transformation_output
