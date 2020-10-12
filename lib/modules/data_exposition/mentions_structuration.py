ce7678765e939313260386c94dd7d06ca179c17c
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 13:53:28 +0200

    (clean) add docstring

--- lib/modules/data_exposition/mentions_structuration.py
+++ lib/modules/data_exposition/mentions_structuration.py
@@ -1,26 +1,26 @@
 
 import pandas as pd
 import json
-from config import EXPOSITION_ZONE_DIRECTORY
+from lib.utils.file import load_json, save_dict_to_json
+from config import EXPOSITION_ZONE_DIRECTORY, OUTPUT_JSON_FILENAME
 
-def run_mentions_reduction(mentions_collection_output):
+def run_mentions_reduction(data_transformation_output:dict):
+    """
+    Collect the ouptut of mentions in pubmed and clinical_trials
+    Reduce the result into one datframe
+    """
     df_all_mentions=pd.DataFrame()
-    for collection_path in mentions_collection_output.values():
+    for collection_path in data_transformation_output.values():
         json_collection = load_json(collection_path)
         df_collection = pd.DataFrame(json_collection["data"])
         df_all_mentions = pd.concat([df_all_mentions,df_collection], axis=0)
     return df_all_mentions
 
 def export_all_mentions(df_all_mentions):
+    """
+    Export the dataframe of all mentions into json
+    """
     df_mentions_todict = df_all_mentions.set_index('id_mention_relation').to_dict(orient='index')
-    output_path= f"{EXPOSITION_ZONE_DIRECTORY}/drugs_all_mentions.json"
+    output_path= f"{EXPOSITION_ZONE_DIRECTORY}/{OUTPUT_JSON_FILENAME}.json"
     save_dict_to_json(df_mentions_todict,output_path)
     return output_path
-
-def load_json(filepath):
-    with open(filepath) as f :
-        res_json=json.load(f)
-    return res_json
-def save_dict_to_json(dictionary, filepath):
-    with open(filepath, 'w') as fp:
-        json.dump(dictionary, fp)
