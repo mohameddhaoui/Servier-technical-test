f1bfdeae7fccd1b647343a7c4d4ef1d5ad3dbe2d
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 12:15:41 +0200

    (clean) delete prints

--- lib/modules/data_prep/data_retrieval.py
+++ lib/modules/data_prep/data_retrieval.py
@@ -46,17 +46,10 @@ def _get_data_source_files_path(datasrc_directory: str, regex_filename: str) -> 
         ---------
              datasrc_regex_files (dict) : List of retrieved files
     """
-    print(datasrc_directory)
     list_all_files_directory = os.listdir(datasrc_directory)
-    print(list_all_files_directory)
     datasrc_regex_files = [
         f"{datasrc_directory}{file_name}"
         for file_name in list_all_files_directory
         if re.match(regex_filename, file_name)
     ]
-    print(datasrc_regex_files)
     return datasrc_regex_files
-
-
-#def get_list_of_files_in_dir(directory: str) -> list:
-#    list_files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
