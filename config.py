82e89513e03e696808fe1f6734b395898bf91cf6
Mohamed DHAOUI <mohamed_dhaoui@carrefour.com>
2020-10-12 09:22:09 +0200

    (feat) add basic config file

---
+++ config.py
@@ -0,0 +1,18 @@
+
+
+LIST_DATASRC = ["drugs","clinical_trials", "pubmed"]
+
+
+MAPPING_TITLE_COLUMN_NAME={"clinical_trials":"title", "pubmed":"scientific_title"}
+TITLE_COLUMN_NAME_TO_USE="title"
+QUARANTINE_ZONE_DIRECTORY = "./data/quarantine/"
+PREPROCESSED_ZONE_DIRECTORY = "./data/preprocessed/"
+EXPOSITION_ZONE_DIRECTORY = "./data/exposition/"
+
+
+
+TITLE_COLUMN_NAME = "title"
+MENTIONS_SOURCES = ["clinical_trials","pubmed"]
+
+DATASRC_CONFIG_PATH = 'configurations/{}_datasrc_config.yaml'
+SUCCESS_STATUS = "success"
